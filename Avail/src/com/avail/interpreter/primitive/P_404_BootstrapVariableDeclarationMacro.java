ad964f7d291dce054313779b88eb631d5a00cc86
Mark van Gulik <mark@availlang.org>
2012-11-03 12:33:14 -0500

    ALL TESTS PASS except macros -renamed method implementation / method site -> method definition. -same for abstract/forward/macro definitions. -more support for macros in AvailCompiler (not finished). -Semicolon is no longer a reserved character; it's just an operator now. -Tweaked some of the old .avail files to compile correctly (in IO and Reflection). -Reworked special objects 41-47. -Mac version no longer clears the dock icon. -Worked on Primitives 400-410 (bootstrap syntax macros).

---
+++ Avail/src/com/avail/interpreter/primitive/P_404_BootstrapVariableDeclarationMacro.java
@@ -0,0 +1,89 @@
+/**
+ * P_404_BootstrapVariableDeclarationMacro.java
+ * Copyright © 1993-2012, Mark van Gulik and Todd L Smith.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avail.interpreter.primitive;
+
+import static com.avail.descriptor.TypeDescriptor.Types.*;
+import static com.avail.descriptor.ParseNodeTypeDescriptor.ParseNodeKind.*;
+import static com.avail.interpreter.Primitive.Flag.*;
+import java.util.*;
+import com.avail.descriptor.*;
+import com.avail.interpreter.*;
+
+/**
+ * The {@code P_404_BootstrapVariableDeclarationMacro} primitive is used
+ * for bootstrapping declaration of a {@link #LOCAL_VARIABLE_NODE local
+ * variable} (without an initializing expression).
+ *
+ * @author Mark van Gulik &lt;mark@availlang.org&gt;
+ */
+public class P_404_BootstrapVariableDeclarationMacro extends Primitive
+{
+	/**
+	 * The sole instance of this primitive class.  Accessed through reflection.
+	 */
+	public final static Primitive instance =
+		new P_404_BootstrapVariableDeclarationMacro().init(
+			2, CannotFail, Bootstrap);
+
+	@Override
+	public Result attempt (
+		final List<AvailObject> args,
+		final Interpreter interpreter)
+	{
+		assert args.size() == 2;
+		final AvailObject variableName = args.get(0);
+		final AvailObject type = args.get(1);
+
+		//TODO[MvG]: Eventually prevent ⊥ from being an accepted type.  For
+		//other bootstrap macro primitives, too.
+
+		final AvailObject variableDeclaration =
+			DeclarationNodeDescriptor.newVariable(
+				variableName,
+				type);
+		variableDeclaration.makeImmutable();
+		return interpreter.primitiveSuccess(variableDeclaration);
+	}
+
+	@Override
+	protected AvailObject privateBlockTypeRestriction ()
+	{
+		return FunctionTypeDescriptor.create(
+			TupleDescriptor.from(
+				/* Variable name token */
+				TOKEN.o(),
+				/* Variable type */
+				InstanceMetaDescriptor.anyMeta()),
+			LOCAL_VARIABLE_NODE.mostGeneralType());
+	}
+}
---
+++ Avail/src/com/avail/interpreter/primitive/P_409_BootstrapVariableUseMacro.java
@@ -0,0 +1,138 @@
+/**
+ * P_408_BootstrapVariableReferenceMacro.java
+ * Copyright © 1993-2012, Mark van Gulik and Todd L Smith.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avail.interpreter.primitive;
+
+import static com.avail.descriptor.TypeDescriptor.Types.*;
+import static com.avail.descriptor.ParseNodeTypeDescriptor.ParseNodeKind.*;
+import static com.avail.interpreter.Primitive.Flag.*;
+import java.util.*;
+import com.avail.compiler.AvailRejectedParseException;
+import com.avail.descriptor.*;
+import com.avail.descriptor.TokenDescriptor.TokenType;
+import com.avail.interpreter.*;
+
+/**
+ * The {@code P_408_BootstrapVariableReferenceMacro} primitive is used to create
+ * {@link ReferenceNodeDescriptor variable reference} phrases.
+ *
+ * @author Mark van Gulik &lt;mark@availlang.org&gt;
+ */
+public class P_409_BootstrapVariableUseMacro extends Primitive
+{
+	/**
+	 * The sole instance of this primitive class.  Accessed through reflection.
+	 */
+	public final static Primitive instance =
+		new P_409_BootstrapVariableUseMacro().init(
+			1, Bootstrap);
+
+	@Override
+	public Result attempt (
+		final List<AvailObject> args,
+		final Interpreter interpreter)
+	{
+		assert args.size() == 1;
+		final AvailObject variableNameLiteral = args.get(0);
+		assert variableNameLiteral.isInstanceOf(LITERAL_NODE.mostGeneralType());
+		final AvailObject literalToken = variableNameLiteral.token();
+		assert literalToken.tokenType() == TokenType.SYNTHETIC_LITERAL;
+		final AvailObject actualToken = literalToken.literal();
+		assert actualToken.isInstanceOf(TOKEN.o());
+
+		if (actualToken.tokenType() != TokenType.KEYWORD)
+		{
+			throw new AvailRejectedParseException(
+				StringDescriptor.from(
+					"(variable name to be a keyword token)"));
+		}
+		final AvailObject variableNameString = actualToken.string();
+		final AvailObject scopeMap =
+			interpreter.currentParserState.scopeMap;
+		if (scopeMap.hasKey(variableNameString))
+		{
+			final AvailObject variableUse = VariableUseNodeDescriptor.newUse(
+				actualToken,
+				scopeMap.mapAt(variableNameString));
+			variableUse.makeImmutable();
+			return interpreter.primitiveSuccess(variableUse);
+		}
+		// Not in a block scope. See if it's a module variable or module
+		// constant...
+		final AvailObject module =
+			interpreter.currentParserState.currentModule();
+		if (module.variableBindings().hasKey(variableNameString))
+		{
+			final AvailObject variableObject = module.variableBindings().mapAt(
+				variableNameString);
+			final AvailObject moduleVarDecl =
+				DeclarationNodeDescriptor.newModuleVariable(
+					actualToken,
+					variableObject,
+					NullDescriptor.nullObject());
+			final AvailObject variableUse = VariableUseNodeDescriptor.newUse(
+				actualToken,
+				moduleVarDecl);
+			variableUse.makeImmutable();
+			return interpreter.primitiveSuccess(variableUse);
+		}
+		if (module.constantBindings().hasKey(variableNameString))
+		{
+			final AvailObject variableObject = module.constantBindings().mapAt(
+				variableNameString);
+			final AvailObject moduleConstDecl =
+				DeclarationNodeDescriptor.newModuleConstant(
+					actualToken,
+					variableObject,
+					NullDescriptor.nullObject());
+			final AvailObject variableUse = VariableUseNodeDescriptor.newUse(
+				actualToken,
+				moduleConstDecl);
+			return interpreter.primitiveSuccess(variableUse);
+		}
+		throw new AvailRejectedParseException(
+			StringDescriptor.from(
+				"variable "
+				+ variableNameString.toString()
+				+ " to be in scope"));
+	}
+
+	@Override
+	protected AvailObject privateBlockTypeRestriction ()
+	{
+		return FunctionTypeDescriptor.create(
+			TupleDescriptor.from(
+				/* Variable name */
+				LITERAL_NODE.create(TOKEN.o())),
+			VARIABLE_USE_NODE.mostGeneralType());
+	}
+}
