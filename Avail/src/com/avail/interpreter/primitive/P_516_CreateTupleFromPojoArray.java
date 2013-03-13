d37ea7dde10b4eee486cf70cfda97c48ccb5d18b
Todd L. Smith <todd@availlang.org>
2013-03-13 15:53:05 -0500

    Added P_516_CreateTupleFromPojoArray.

---
+++ Avail/src/com/avail/interpreter/primitive/P_516_CreateTupleFromPojoArray.java
@@ -0,0 +1,85 @@
+/**
+ * P_516_CreateTupleFromPojoArray.java
+ * Copyright © 1993-2012, Mark van Gulik and Todd L Smith.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avail.interpreter.primitive;
+
+import static com.avail.interpreter.Primitive.Flag.*;
+import java.util.List;
+import com.avail.annotations.NotNull;
+import com.avail.descriptor.*;
+import com.avail.interpreter.*;
+import com.avail.utility.*;
+
+/**
+ * <strong>Primitive 516</strong>: Convert the specified {@linkplain
+ * PojoTypeDescriptor#mostGeneralArrayType() pojo array} to a {@linkplain
+ * TupleDescriptor tuple}.
+ *
+ * @author Todd L Smith &lt;todd@availlang.org&gt;
+ */
+public final class P_516_CreateTupleFromPojoArray
+extends Primitive
+{
+	/**
+	 * The sole instance of this primitive class. Accessed through reflection.
+	 */
+	public final @NotNull static Primitive instance =
+		new P_516_CreateTupleFromPojoArray().init(1, CanInline, CannotFail);
+
+	@Override
+	public Result attempt (
+		final List<AvailObject> args,
+		final Interpreter interpreter)
+	{
+		assert args.size() == 1;
+		final AvailObject array = args.get(0);
+		final Mutable<AvailObject> tuple = new Mutable<AvailObject>();
+		array.lock(new Continuation0()
+		{
+			@Override
+			public void value ()
+			{
+				tuple.value = TupleDescriptor.from(array);
+			}
+		});
+		return interpreter.primitiveSuccess(tuple.value);
+	}
+
+	@Override
+	protected AvailObject privateBlockTypeRestriction ()
+	{
+		return FunctionTypeDescriptor.create(
+			TupleDescriptor.from(
+				PojoTypeDescriptor.mostGeneralArrayType()),
+			TupleTypeDescriptor.mostGeneralType());
+	}
+}
