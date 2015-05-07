164eb9bb0f0c62da12535456852d6436a7fd85aa
Mark van Gulik <mark@availlang.org>
2015-05-07 15:49:14 -0500

    Forgot two files.

---
+++ Avail/src/com/avail/compiler/AvailAcceptedParseException.java
@@ -0,0 +1,63 @@
+/**
+ * AvailAcceptedParseException.java
+ * Copyright © 1993-2015, The Avail Foundation, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avail.compiler;
+
+import com.avail.exceptions.PrimitiveThrownException;
+import com.avail.interpreter.primitive.P_343_AcceptParsing;
+
+/**
+ * An {@code AvailAcceptedParseException} is thrown by primitive {@link
+ * P_343_AcceptParsing} to indicate the fiber running a semantic restriction
+ * has accepted the argument types and does not need to restrict the proposed
+ * expression's type.
+ *
+ * @author Mark van Gulik &lt;mark@availlang.org&gt;
+ */
+public class AvailAcceptedParseException
+extends PrimitiveThrownException
+{
+	/**
+	 * The serial version identifier.
+	 */
+	private static final long serialVersionUID = 1956015610046321084L;
+
+	/**
+	 * Construct a new {@link AvailAcceptedParseException}, which can be thrown
+	 * by primitive {@link P_343_AcceptParsing} to indicate a semantic
+	 * restriction has accepted a parse but does not wish to strengthen the
+	 * exrpssion's type.
+	 */
+	public AvailAcceptedParseException ()
+	{
+	}
+}
