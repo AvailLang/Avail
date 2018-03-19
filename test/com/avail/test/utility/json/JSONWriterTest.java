219e765c1866d997626e84453b81ba55a8182f87
richATavail <rich@availlang.org>
2018-03-19 15:40:48 -0500

    Added Avail JSON utility tests. Update comment to clarify a point in JSONWriter.

---
+++ test/com/avail/test/utility/json/JSONWriterTest.java
@@ -0,0 +1,124 @@
+/*
+ * JSONWriterTest.java
+ * Copyright © 1993-2018, The Avail Foundation, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avail.test.utility.json;
+import com.avail.utility.json.JSONException;
+import com.avail.utility.json.JSONObject;
+import com.avail.utility.json.JSONReader;
+import com.avail.utility.json.JSONWriter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import static com.avail.test.utility.json.TestJSONKeyValue.*;
+import static com.avail.test.utility.json.TestJSONKeyValue.OBJINT;
+import static com.avail.utility.Nulls.stripNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * A {@code JSONWriterTest} contains unit tests for the {@link JSONWriter}.
+ *
+ * @author Richard Arriaga &lt;rich@availlang.org&gt;
+ */
+public class JSONWriterTest
+{
+	/**
+	 * Answer a {@link JSONObject} from the provided {@link StringBuilder}.
+	 *
+	 * @param writer
+	 *        The {@code StringBuilder} that contains the JSON payload.
+	 * @return A {@code JSONObject}.
+	 */
+	private static JSONObject getJsonData (final JSONWriter writer)
+	{
+		try (final JSONReader reader = new JSONReader(
+			new StringReader(writer.toString())))
+		{
+			final JSONObject content = (JSONObject) reader.read();
+			assert content != null : "The payload should not be empty!";
+			return content;
+		}
+		catch (final JSONException e)
+		{
+			fail(String.format(
+				"The following test JSON could not be parsed:\n%s",
+				writer.contents()));
+		}
+		catch (final IllegalStateException e)
+		{
+			fail(String.format(
+				"The following test JSON could not be created due to an "
+					+ "IOException:\n%s",
+				writer.contents()));
+		}
+		catch (final IOException e)
+		{
+			fail(String.format(
+				"The following test JSON could not be created due to an "
+					+ "IOException:\n%s",
+				writer.contents()));
+		}
+		return null; // Shouldn't get here
+	}
+
+	/**
+	 * Display the test payload to screen.
+	 *
+	 * @param writer
+	 *        The {@link JSONWriter} with the test JSON.
+	 */
+	private static void displayTestPayload (final JSONWriter writer)
+	{
+		System.out.println("Test Payload\n============\n" + writer + "\n");
+	}
+
+	@Test
+	@DisplayName("Correctly built JSON")
+	void correctCompactJSONTest ()
+	{
+		final JSONWriter writer = new JSONWriter();
+		writer.startObject();
+		addToWriter(writer, IMASTRING, IMANINT, IMALONG, IMAFLOAT, IMATRUE,
+			IMAFALSE, IMANULL, IMACOMPACTARRAY);
+		IMASTRING.addValueToWriter(writer);
+		addIMANOBJECTFields(writer, OBJSTRING, OBJINT);
+		writer.endObject();
+		final JSONObject content = getJsonData(writer);
+		test(stripNull(content), IMASTRING, IMANINT, IMALONG, IMAFLOAT,
+			IMATRUE, IMAFALSE, IMANULL, IMACOMPACTARRAY, IMANOBJECT);
+		final JSONObject objContent = content.getObject(IMANOBJECT.key);
+		test(objContent, OBJSTRING, OBJINT);
+		displayTestPayload(writer);
+	}
+}
