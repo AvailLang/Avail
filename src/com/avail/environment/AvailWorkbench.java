b3f4a8c79d72f3cf50a3edfd2fa6d23d3d2a7382
Mark van Gulik <mark@availlang.org>
2018-05-25 05:36:29 -0500

    Removed residual JavaFX reference for issue #123.

--- src/com/avail/environment/AvailWorkbench.java
+++ src/com/avail/environment/AvailWorkbench.java
@@ -52,7 +52,6 @@ import com.avail.stacks.StacksGenerator;
 import com.avail.utility.IO;
 import com.avail.utility.Mutable;
 import com.avail.utility.Pair;
-import com.sun.javafx.application.PlatformImpl;
 
 import javax.annotation.Nullable;
 import javax.swing.*;
@@ -2440,11 +2439,6 @@ extends JFrame
 	@InnerAccess AvailWorkbench (
 		final ModuleNameResolver resolver)
 	{
-		//This must be called to circumvent a bug that won't be fixed for swing
-		//and JavaFx interaction
-		//See https://bugs.openjdk.java.net/browse/JDK-8090517
-		PlatformImpl.startup(() -> {});
-
 		// Set module components.
 		this.resolver = resolver;
 		final AvailRuntime runtime = new AvailRuntime(resolver);
