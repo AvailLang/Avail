4da294ed5e8282097343401447e8400ccaf10306
Todd L. Smith <todd@availlang.org>
2019-08-16 19:11:29 -0500

    Updated Windows batch file for running Avail workbench.

--- distro/bin/Windows/AvailWorkbench.bat
+++ distro/bin/Windows/AvailWorkbench.bat
@@ -1,21 +1,13 @@
-REM ***  Launches the Avail workbench.  Adjust as needed, such as the path to
-REM ***  the Avail home directory.
+REM ***  Launches the Avail workbench.  Assumes %AVAIL_HOM% is already set.
 
-set AVAIL_HOME=C:\Users\Avail\workspace\Avail
-set temp_cp=%AVAIL_HOME%\distro\lib\AvailDev.jar
-set temp_cp=%temp_cp%;%AVAIL_HOME%\distro\lib\Avail.jar
-set temp_cp=%temp_cp%;%AVAIL_HOME%\distro\lib\AvailServer.jar
-set temp_cp=%temp_cp%;%AVAIL_HOME%\libraries\asm-6.0.jar
-set temp_cp=%temp_cp%;%AVAIL_HOME%\libraries\asm-analysis-6.0.jar
-set temp_cp=%temp_cp%;%AVAIL_HOME%\libraries\asm-util-6.0.jar
-set temp_cp=%temp_cp%;%AVAIL_HOME%\libraries\jsr305.jar
+set temp_cp=%AVAIL_HOME%\distro\lib\avail-workbench-1.4.jar
 
 REM ***  This sets up the "avail" and "examples" roots.  Editing and saving
 REM ***  with the Preferences window will update the Windows per-user app data,
 REM ***  which will only be used next time if the (-D)availRoots definition is
 REM ***  removed from the command line.
 
-set temp_roots=avail=%AVAIL_HOME%\repositories\avail.repo,%AVAIL_HOME%\distro\src\avail
-set temp_roots=%temp_roots%;examples=%AVAIL_HOME%\repositories\examples.repo,%AVAIL_HOME%\distro\src\examples
+set temp_roots=avail=%HOMEPATH%\repositories\avail.repo,%AVAIL_HOME%\distro\src\avail
+set temp_roots=%temp_roots%;examples=%HOMEPATH%\repositories\examples.repo,%AVAIL_HOME%\distro\src\examples
 
-java -Xmx2g -DavailRoots="%temp_roots%" -cp "%temp_cp%" com.avail.environment.AvailWorkbench
+java -Xmx4g -DavailRoots="%temp_roots%" -cp "%temp_cp%" com.avail.environment.AvailWorkbench
