1687e0085f85da74f41864e1aa0dc360429ac694
Richard Arriaga <richard.arriaga@i3pd.com>
2019-10-18 18:00:06 -0500

    Workbench port to Kotlin WIP. AvailWorkbench still busted due to lack of native methods (e.g. notifyAll)

---
+++ avail-workbench/src/main/java/com/avail/environment/editor/utility/PrefixTrie.kt
@@ -0,0 +1,116 @@
+package com.avail.environment.editor.utility
+
+import com.avail.utility.Pair
+import com.avail.utility.Nulls.stripNull
+
+/**
+ * A `PrefixTrie` is trie of [PrefixNode]s.
+ *
+ * @param <T>
+ * The type of object potentially held at the `PrefixNode`.
+ * @author Rich Arriaga &lt;rich@availlang.org&gt;
+</T> */
+class PrefixTrie<T>
+{
+	/**
+	 * The root [PrefixNode].
+	 */
+	val root = PrefixNode<T>(-1)
+
+	/**
+	 * A lock for concurrent updates to this trie.
+	 */
+	private val lock = Any()
+
+	/**
+	 * Add a template to this `PrefixTrie`.
+	 *
+	 * @param word
+	 *   The search text, the characters that will functionType the
+	 *   [PrefixNode]s.
+	 * @param content
+	 *   The [content][PrefixNode.content].
+	 */
+	fun addWord(word: String, content: T)
+	{
+		var addWord = false
+		synchronized(lock)
+		{
+			val startSize = root.words.size
+			root.words.add(word.toLowerCase())
+			if (root.words.size > startSize)
+			{
+				addWord = true
+			}
+		}
+		if (addWord)
+		{
+			root.addWord(word.toLowerCase(), content)
+		}
+	}
+
+	/**
+	 * Answer the [PrefixNode] for the given String.
+	 *
+	 * @param word
+	 *   The String to find.
+	 * @param acceptEachNode
+	 *   A lambda that accepts a [PrefixNode].
+	 * @return A `PrefixNode` if one exists; `null` otherwise.
+	 */
+	fun searchNode(word: String, acceptEachNode: (PrefixNode<T>?) -> Unit)
+		: PrefixNode<T>? = root.searchTrie(word, acceptEachNode)
+
+	/**
+	 * Answer the [PrefixNode] for the given String.
+	 *
+	 * @param word
+	 * The String to find.
+	 * @return A `PrefixNode` if one exists; `null` otherwise.
+	 */
+	fun searchNode(word: String): PrefixNode<T>? = root.searchTrie(word)
+
+	/**
+	 * Answer a [List] of [Pair]s of word and corresponding
+	 * [content][PrefixNode.content] in this `PrefixTrie`.
+	 *
+	 * @return A list.
+	 */
+	fun wordContent(): List<NodeContent<T>>
+	{
+		val wordTemplates = ArrayList<NodeContent<T>>()
+		root.words().forEach { word ->
+			val nodeContent = stripNull(searchNode(word)).content()
+			if (nodeContent != null)
+			{
+				wordTemplates.add(
+					NodeContent(word, nodeContent))
+			}
+		}
+
+		return wordTemplates
+	}
+
+	/**
+	 * A pairing of the [PrefixNode] word up to that node and the
+	 * [content][PrefixNode.content] at the node.
+	 *
+	 * @param T
+	 *   The type of content contained in the node.
+	 *
+	 * @property word
+	 *   The word spelled up to the originating [PrefixNode].
+	 * @property content
+	 *   The content at the originating [PrefixNode].
+	 *
+	 * @constructor
+	 * Construct a `NodeContent`.
+	 *
+	 * @param word
+	 *   The search word.
+	 * @param content
+	 *   The [content][PrefixNode.content].
+	 */
+	data class NodeContent<T> internal constructor(
+		val word: String, val content: T)
+}
---
+++ avail-workbench/src/main/java/com/avail/environment/tasks/UnloadTask.kt
@@ -0,0 +1,71 @@
+/*
+ * UnloadTask.java
+ * Copyright © 1993-2018, The Avail Foundation, LLC.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package com.avail.environment.tasks
+
+import com.avail.builder.ResolvedModuleName
+import com.avail.descriptor.ModuleDescriptor
+import com.avail.environment.AvailWorkbench
+import com.avail.environment.AvailWorkbench.AbstractWorkbenchTask
+import java.awt.*
+
+/**
+ * An `UnloadTask` initiates and manages unloading the target
+ * [module][ModuleDescriptor].
+ *
+ * @constructor
+ * Construct a new `UnloadTask`.
+ *
+ * @param workbench
+ *  The owning [AvailWorkbench].
+ * @param targetModuleName
+ *  The resolved name of the target [module][ModuleDescriptor] to unload, or
+ *  null to unload all modules.
+ */
+class UnloadTask constructor(
+		workbench: AvailWorkbench, targetModuleName: ResolvedModuleName?)
+	: AbstractWorkbenchTask(workbench, targetModuleName)
+{
+	override fun executeTask()
+	{
+		workbench.availBuilder.unloadTarget(targetModuleName)
+	}
+
+	override fun done()
+	{
+		workbench.backgroundTask = null
+		reportDone()
+		workbench.availBuilder.checkStableInvariants()
+		workbench.setEnablements()
+		workbench.cursor = Cursor.getDefaultCursor()
+	}
+}
