a3b79c4e14846ad0b60675e47607cb51787d33b3
Mark van Gulik <mark@availlang.org>
2020-03-30 01:51:40 -0500

    Fully Kotlinized BitField.

--- src/main/kotlin/com/avail/descriptor/representation/BitField.kt
+++ src/main/kotlin/com/avail/descriptor/representation/BitField.kt
@@ -35,77 +35,83 @@ import com.avail.annotations.EnumField
 import com.avail.descriptor.AbstractDescriptor
 
 /**
- * A `RuntimeBitField` is constructed at class loading time and contains
- * any cached information needed to access a range of up to 32 contiguous bits
+ * A `BitField` is constructed at class loading time and contains any cached
+ * information needed to efficiently access a range of up to 32 contiguous bits
  * from an [integer slot][IntegerSlotsEnum].
  *
+ * @constructor
+ * @property integerSlot
+ *   The [integer slot][IntegerSlotsEnum] within which this `BitField`` occurs.
+ * @property shift
+ *   The lowest bit position that this BitField occupies.  Zero (`0`) is the
+ *   rightmost or lowest order bit.
+ * @property bits
+ *   The number of bits that this `BitField` occupies within a [Long].
  * @author Mark van Gulik &lt;mark@availlang.org&gt;
  */
-class BitField(
-	integerSlot: IntegerSlotsEnum,
-	shift: Int,
-	bits: Int) : Comparable<BitField?> {
-	/**
-	 * The [integer slot][IntegerSlotsEnum] within which this bit field
-	 * occurs.
-	 */
+class BitField (
 	@JvmField
-	val integerSlot: IntegerSlotsEnum
+	val integerSlot: IntegerSlotsEnum,
+	@JvmField
+	val shift: Int,
+	@JvmField
+	val bits: Int
+) : Comparable<BitField?> {
+	init {
+		assert(shift == shift and 63)
+		assert(bits > 0)
+		assert(bits <= 32) // Must use at most 32 bits of the long.
+		assert(shift + bits <= 64)
+	}
 
 	/**
 	 * The zero-based [integer slot][IntegerSlotsEnum] within which this
 	 * bit field occurs.
 	 */
-	val integerSlotIndex: Int
-
-	/**
-	 * The lowest bit position that this BitField occupies.  Zero (`0`) is
-	 * the rightmost or lowest order bit.
-	 */
-	private val shift: Int
-
-	/**
-	 * The number of bits that this BitField occupies within a `long`.
-	 */
-	@JvmField
-	val bits: Int
+	val integerSlotIndex = integerSlot.fieldOrdinal()
 
 	/**
 	 * A string of 1's of length [.bits], right aligned in the `long`.  Even though bit fields can't be more than 32 long, it's a long
 	 * to avoid sign extension problems.
 	 */
-	private val lowMask: Long
+	private val lowMask = (1L shl bits) - 1
 
 	/**
 	 * An integer containing 1-bits in exactly the positions reserved for this
 	 * bit field.
 	 */
-	private val mask: Long
+	private val mask = lowMask shl shift
 
 	/**
 	 * An integer containing 0-bits in exactly the positions reserved for this
 	 * bit field.
 	 */
-	private val invertedMask: Long
+	private val invertedMask = mask.inv()
 
 	/**
-	 * The name of this `BitField`.  This is filled in as needed by the
-	 * default [object printing][AbstractDescriptor.printObjectOnAvoidingIndent]
+	 * The name of this `BitField`.  This is filled in as needed by the default
+	 * [object&#32;printing][AbstractDescriptor.printObjectOnAvoidingIndent]
 	 * mechanism.
 	 */
 	@JvmField
 	var name: String? = null
 
 	/**
-	 * The [EnumField] with which this `BitField` is annotated, if
-	 * any.  This is populated by the default [ ][AbstractDescriptor.printObjectOnAvoidingIndent] mechanism.
+	 * The [EnumField] with which this `BitField` is annotated, if any.  This is
+	 * populated by the default [
+	 * ][AbstractDescriptor.printObjectOnAvoidingIndent] mechanism.
 	 */
 	@JvmField
 	var enumField: EnumField? = null
+
+	/**
+	 * Compare two `BitFields` for sorting. They must be for the same slot.
+	 * Order by descending shift values.
+	 */
 	override fun compareTo(other: BitField?): Int {
-		assert(other != null)
-		assert(integerSlot === other!!.integerSlot) { "Bit fields of different slots are incomparable" }
-		// Order by descending shift values.
+		assert(integerSlot === other!!.integerSlot) {
+			"Bit fields of different slots are incomparable"
+		}
 		return Integer.compare(other!!.shift, shift)
 	}
 
@@ -113,68 +119,42 @@ class BitField(
 	 * Answer whether the receiver and the passed `BitField` occupy the
 	 * same span of bits at the same integer slot number.
 	 *
-	 * @param bitField The other `BitField`.
-	 * @return Whether the two BitFields have the same slot number, start bit,
-	 * and end bit.
+	 * @param bitField
+	 *   The other `BitField`.
+	 * @return
+	 *   Whether the two BitFields have the same slot number, start bit, and end
+	 *   bit.
 	 */
-	fun isSamePlaceAs(bitField: BitField): Boolean {
-		return integerSlotIndex == bitField.integerSlotIndex && shift == bitField.shift && bits == bitField.bits
-	}
+	fun isSamePlaceAs(bitField: BitField) =
+		integerSlotIndex == bitField.integerSlotIndex
+			&& shift == bitField.shift
+			&& bits == bitField.bits
 
-	override fun toString(): String {
-		return String.format(
-			"%s(%d:%d)",
-			javaClass.simpleName,
-			shift,
-			bits)
-	}
+	override fun toString() = "${javaClass.simpleName}($shift:$bits)"
 
 	/**
-	 * Extract this `BitField` from the given `long`.
-	 * @param longValue A long.
-	 * @return An `int` extracted from the range of bits this `BitField` occupies.
-	 */
-	fun extractFromLong(longValue: Long): Int {
-		return (longValue ushr shift and lowMask).toInt()
-	}
-
-	/**
-	 * Given a `long` field value, replace the bits occupied by this
-	 * `BitField` with the supplied `int` value, returning the
-	 * resulting `long`.
-	 * @param longValue A `long` into which to replace the bit field.
-	 * @param bitFieldValue The `int` to be written into the bit field.
-	 * @return The `long` with the bit field updated.
-	 */
-	fun replaceBits(longValue: Long, bitFieldValue: Int): Long {
-		return (longValue and invertedMask
-			or (bitFieldValue.toLong() shl shift and mask))
-	}
-
-	/**
-	 * Construct a new `BitField`.
+	 * Extract this `BitField` from the given [Long].
 	 *
-	 * @param integerSlot
-	 * The [integer slot][IntegerSlotsEnum] in which this bit field
-	 * occurs.
-	 * @param shift
-	 * The bit position of the rightmost (lowest order) bit occupied by
-	 * this `BitField`.
-	 * @param bits
-	 * The number of bits this `BitField` occupies.
+	 * @param longValue
+	 *   A [Long].
+	 * @return
+	 *   An [Int] extracted from the range of bits this `BitField` occupies.
 	 */
-	init {
-		assert(shift == shift and 63)
-		assert(bits > 0)
-		assert(bits <= 32 // Must use at most 32 bits of the long.
-		)
-		assert(shift + bits <= 64)
-		this.integerSlot = integerSlot
-		this.shift = shift
-		this.bits = bits
-		integerSlotIndex = integerSlot.fieldOrdinal()
-		lowMask = (1L shl bits) - 1
-		mask = lowMask shl shift
-		invertedMask = mask.inv()
-	}
+	fun extractFromLong(longValue: Long) =
+		(longValue ushr shift and lowMask).toInt()
+
+	/**
+	 * Given a [Long] field value, replace the bits occupied by this `BitField`
+	 * with the supplied [Int] value, returning the resulting [Long].
+	 *
+	 * @param longValue
+	 *   A [Long] into which to replace the bit field.
+	 * @param bitFieldValue
+	 *   The [Int] to be written into the bit field.
+	 * @return
+	 *   The [Long] with the bit field updated.
+	 */
+	fun replaceBits(longValue: Long, bitFieldValue: Int) =
+		(longValue and invertedMask
+			or (bitFieldValue.toLong() shl shift and mask))
 }
