cf2e15114fabacf9dc8bfddc7b0ac1cd9470e319
Richard Arriaga <richard.arriaga@i3pd.com>
2020-06-05 16:13:30 -0500

    Converted A_String to Kotlin. All tests pass.

--- src/main/java/com/avail/descriptor/tuples/A_String.kt
+++ src/main/java/com/avail/descriptor/tuples/A_String.kt
@@ -1,19 +1,19 @@
 /*
- * A_String.java
+ * A_String.kt
  * Copyright © 1993-2019, The Avail Foundation, LLC.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *  Redistributions of source code must retain the above copyright notice, this
+ * * Redistributions of source code must retain the above copyright notice, this
  *   list of conditions and the following disclaimer.
  *
- *  Redistributions in binary form must reproduce the above copyright notice,
+ * * Redistributions in binary form must reproduce the above copyright notice,
  *   this list of conditions and the following disclaimer in the documentation
  *   and/or other materials provided with the distribution.
  *
- *  Neither the name of the copyright holder nor the names of the contributors
+ * * Neither the name of the copyright holder nor the names of the contributors
  *   may be used to endorse or promote products derived from this software
  *   without specific prior written permission.
  *
@@ -29,43 +29,45 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+package com.avail.descriptor.tuples
 
-package com.avail.descriptor.tuples;
-
-import com.avail.descriptor.representation.A_BasicObject;
-import com.avail.descriptor.representation.AvailObject;
+import com.avail.descriptor.representation.A_BasicObject
+import com.avail.descriptor.representation.AvailObject
 
 /**
- * {@code A_String} is an interface that specifies the string-specific operations that an {@link AvailObject} must implement.  It's a sub-interface of {@link A_Tuple} (which is itself a sub-interface of {@link A_BasicObject}.
+ * `A_String` is an interface that specifies the string-specific operations that
+ * an [AvailObject] must implement.  It's a sub-interface of [A_Tuple] (which is
+ * itself a sub-interface of [A_BasicObject].
  *
  * @author Mark van Gulik &lt;mark@availlang.org&gt;
  */
-public interface A_String
-extends A_Tuple
+interface A_String : A_Tuple
 {
 	/**
-	 * Construct a Java {@linkplain String string} from the receiver, an Avail
-	 * {@linkplain StringDescriptor string}.
+	 * Construct a Java [string][String] from the receiver, an Avail
+	 * [string][StringDescriptor].
 	 *
 	 * @return
-	 * The corresponding Java string.
+	 *   The corresponding Java string.
 	 */
-	String asNativeString ();
+	fun asNativeString(): String
 
 	/**
-	 * Even though {@link #copyTupleFromToCanDestroy(int, int, boolean)} would perform the same activity, this method returns the stronger {@code A_String} type as a convenience, when the code knows it's working on strings.
+	 * Even though [copyTupleFromToCanDestroy] would perform the same activity,
+	 * this method returns the stronger `A_String` type as a convenience, when
+	 * the code knows it's working on strings.
 	 *
 	 * @param start
-	 *        The start of the range to extract.
+	 *   The start of the range to extract.
 	 * @param end
-	 *        The end of the range to extract.
+	 *   The end of the range to extract.
 	 * @param canDestroy
-	 *        Whether the original object may be destroyed if mutable.
+	 *   Whether the original object may be destroyed if mutable.
 	 * @return
-	 * The substring.
+	 *   The substring.
 	 */
-	A_String copyStringFromToCanDestroy (
-		final int start,
-		final int end,
-		final boolean canDestroy);
+	fun copyStringFromToCanDestroy(
+		start: Int,
+		end: Int,
+		canDestroy: Boolean): A_String
 }
