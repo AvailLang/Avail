9f0cddb283374e1388e9b76d4fb24bcbf3ac83e5
Richard Arriaga <richard.arriaga@i3pd.com>
2020-06-08 23:34:09 -0500

    TypeTag ported to Kotlin All tests passed.

--- src/main/java/com/avail/descriptor/types/TypeTag.kt
+++ src/main/java/com/avail/descriptor/types/TypeTag.kt
@@ -6,16 +6,16 @@
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  *
- *  * Redistributions of source code must retain the above copyright notice, this
- *     list of conditions and the following disclaimer.
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
  *
- *  * Redistributions in binary form must reproduce the above copyright notice, this
- *     list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
  *
- *  * Neither the name of the copyright holder nor the names of the contributors
- *    may be used to endorse or promote products derived from this software
- *    without specific prior written permission.
+ * * Neither the name of the copyright holder nor the names of the contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
@@ -29,17 +29,12 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+package com.avail.descriptor.types
 
-package com.avail.descriptor.types;
-
-import javax.annotation.Nullable;
-import java.util.ArrayList;
-import java.util.List;
-
-import static com.avail.utility.Nulls.stripNull;
+import com.avail.utility.Nulls
 
 /**
- * {@code TypeTag} is an enumeration that corresponds with the basic type
+ * `TypeTag` is an enumeration that corresponds with the basic type
  * structure of Avail's type lattice.  Even though the type lattice contains an
  * infinite collection of infinitely long chains of types, some of which have
  * an infinite number of direct ancestors and direct descendants, we're still
@@ -56,200 +51,207 @@ import static com.avail.utility.Nulls.stripNull;
  *
  * @author Mark van Gulik &lt;mark@availlang.org&gt;
  */
-@SuppressWarnings("JavaDoc")
-public enum TypeTag
+enum class TypeTag
 {
-	UNKNOWN_TAG(),
-	TOP_TAG(),
-		NIL_TAG(TOP_TAG),
-		ATOM_TAG(TOP_TAG),
-			BOOLEAN_TAG(ATOM_TAG),
-				TRUE_TAG(BOOLEAN_TAG),
-				FALSE_TAG(BOOLEAN_TAG),
-		BUNDLE_TAG(TOP_TAG),
-		BUNDLE_TREE_TAG(TOP_TAG),
-		CHARACTER_TAG(TOP_TAG),
-		CONTINUATION_TAG(TOP_TAG),
-		DEFINITION_TAG(TOP_TAG),
-		FIBER_TAG(TOP_TAG),
-		FUNCTION_TAG(TOP_TAG),
-		GRAMMATICAL_RESTRICTION_TAG(TOP_TAG),
-		LEXER_TAG(TOP_TAG),
-		MAP_TAG(TOP_TAG),
-		MAP_LINEAR_BIN_TAG(TOP_TAG),
-		MAP_HASHED_BIN_TAG(TOP_TAG),
-		METHOD_TAG(TOP_TAG),
-		MODULE_TAG(TOP_TAG),
-		NUMBER_TAG(TOP_TAG),
-			EXTENDED_INTEGER_TAG(NUMBER_TAG),
-				INTEGER_TAG(EXTENDED_INTEGER_TAG),
-					WHOLE_NUMBER_TAG(INTEGER_TAG),
-						NATURAL_NUMBER_TAG(WHOLE_NUMBER_TAG),
-				NEGATIVE_INFINITY_TAG(EXTENDED_INTEGER_TAG),
-				POSITIVE_INFINITY_TAG(EXTENDED_INTEGER_TAG),
-			FLOAT_TAG(NUMBER_TAG),
-			DOUBLE_TAG(NUMBER_TAG),
-		OBJECT_TAG(TOP_TAG),
-		PARSING_PLAN_TAG(TOP_TAG),
-		PARSING_PLAN_IN_PROGRESS_TAG(TOP_TAG),
-		PHRASE_TAG(TOP_TAG),
-			MARKER_PHRASE_TAG(PHRASE_TAG),
-			EXPRESSION_PHRASE_TAG(PHRASE_TAG),
-				ASSIGNMENT_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-				BLOCK_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-				LITERAL_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-				REFERENCE_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-				SUPER_CAST_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-				SEND_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-				LIST_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-					PERMUTED_LIST_PHRASE_TAG(LIST_PHRASE_TAG),
-				VARIABLE_USE_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
-			STATEMENT_PHRASE_TAG(PHRASE_TAG),
-				SEQUENCE_PHRASE_TAG(STATEMENT_PHRASE_TAG),
-				FIRST_OF_SEQUENCE_PHRASE_TAG(STATEMENT_PHRASE_TAG),
-				DECLARATION_PHRASE_TAG(STATEMENT_PHRASE_TAG),
-					ARGUMENT_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-					LABEL_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-					LOCAL_VARIABLE_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-					LOCAL_CONSTANT_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-					MODULE_VARIABLE_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-					MODULE_CONSTANT_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-					PRIMITIVE_FAILURE_REASON_PHRASE_TAG(DECLARATION_PHRASE_TAG),
-				EXPRESSION_AS_STATEMENT_PHRASE_TAG(STATEMENT_PHRASE_TAG),
-			MACRO_SUBSTITUTION_PHRASE_TAG(PHRASE_TAG),
-		POJO_TAG(TOP_TAG),
-		RAW_FUNCTION_TAG(TOP_TAG),
-		SEMANTIC_RESTRICTION_TAG(TOP_TAG),
-		SET_TAG(TOP_TAG),
-		SET_LINEAR_BIN_TAG(TOP_TAG),
-		SET_HASHED_BIN_TAG(TOP_TAG),
-		TOKEN_TAG(TOP_TAG),
-			LITERAL_TOKEN_TAG(TOKEN_TAG),
-		TUPLE_TAG(TOP_TAG),
-			STRING_TAG(TUPLE_TAG),
-		VARIABLE_TAG(TOP_TAG),
-		TOP_TYPE_TAG(TOP_TAG, TOP_TAG),
-			ANY_TYPE_TAG(TOP_TYPE_TAG),
-				NONTYPE_TYPE_TAG(ANY_TYPE_TAG),
-					SET_TYPE_TAG(NONTYPE_TYPE_TAG, SET_TAG),
-					POJO_TYPE_TAG(NONTYPE_TYPE_TAG, POJO_TAG),
-					NUMBER_TYPE_TAG(NONTYPE_TYPE_TAG, NUMBER_TAG),
-						EXTENDED_INTEGER_TYPE_TAG(
-								NUMBER_TYPE_TAG, EXTENDED_INTEGER_TAG),
-					PHRASE_TYPE_TAG(NONTYPE_TYPE_TAG, PHRASE_TAG),
-						LIST_PHRASE_TYPE_TAG(PHRASE_TYPE_TAG, LIST_PHRASE_TAG),
-					VARIABLE_TYPE_TAG(NONTYPE_TYPE_TAG, VARIABLE_TAG),
-					PRIMITIVE_TYPE_TAG(NONTYPE_TYPE_TAG),
-					FUNCTION_TYPE_TAG(NONTYPE_TYPE_TAG, FUNCTION_TAG),
-					OBJECT_TYPE_TAG(NONTYPE_TYPE_TAG, OBJECT_TAG),
-					MAP_TYPE_TAG(NONTYPE_TYPE_TAG, MAP_TAG),
-					TUPLE_TYPE_TAG(NONTYPE_TYPE_TAG, TUPLE_TAG),
-					CONTINUATION_TYPE_TAG(NONTYPE_TYPE_TAG, CONTINUATION_TAG),
-					RAW_FUNCTION_TYPE_TAG(NONTYPE_TYPE_TAG, RAW_FUNCTION_TAG),
-					FIBER_TYPE_TAG(NONTYPE_TYPE_TAG, FIBER_TAG),
-				META_TAG(ANY_TYPE_TAG, TOP_TYPE_TAG),
-				BOTTOM_TYPE_TAG(ANY_TYPE_TAG);  // Special case
+	UNKNOWN_TAG,
+	TOP_TAG,
+	NIL_TAG(TOP_TAG),
+	ATOM_TAG(TOP_TAG),
+	BOOLEAN_TAG(ATOM_TAG),
+	TRUE_TAG(BOOLEAN_TAG),
+	FALSE_TAG(BOOLEAN_TAG),
+	BUNDLE_TAG(TOP_TAG),
+	BUNDLE_TREE_TAG(TOP_TAG),
+	CHARACTER_TAG(TOP_TAG),
+	CONTINUATION_TAG(TOP_TAG),
+	DEFINITION_TAG(TOP_TAG),
+	FIBER_TAG(TOP_TAG),
+	FUNCTION_TAG(TOP_TAG),
+	GRAMMATICAL_RESTRICTION_TAG(TOP_TAG),
+	LEXER_TAG(TOP_TAG),
+	MAP_TAG(TOP_TAG),
+	MAP_LINEAR_BIN_TAG(TOP_TAG),
+	MAP_HASHED_BIN_TAG(TOP_TAG),
+	METHOD_TAG(TOP_TAG),
+	MODULE_TAG(TOP_TAG),
+	NUMBER_TAG(TOP_TAG),
+	EXTENDED_INTEGER_TAG(NUMBER_TAG),
+	INTEGER_TAG(EXTENDED_INTEGER_TAG),
+	WHOLE_NUMBER_TAG(INTEGER_TAG),
+	NATURAL_NUMBER_TAG(WHOLE_NUMBER_TAG),
+	NEGATIVE_INFINITY_TAG(EXTENDED_INTEGER_TAG),
+	POSITIVE_INFINITY_TAG(EXTENDED_INTEGER_TAG),
+	FLOAT_TAG(NUMBER_TAG),
+	DOUBLE_TAG(NUMBER_TAG),
+	OBJECT_TAG(TOP_TAG),
+	PARSING_PLAN_TAG(TOP_TAG),
+	PARSING_PLAN_IN_PROGRESS_TAG(TOP_TAG),
+	PHRASE_TAG(TOP_TAG),
+	MARKER_PHRASE_TAG(PHRASE_TAG),
+	EXPRESSION_PHRASE_TAG(PHRASE_TAG),
+	ASSIGNMENT_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	BLOCK_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	LITERAL_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	REFERENCE_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	SUPER_CAST_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	SEND_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	LIST_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	PERMUTED_LIST_PHRASE_TAG(LIST_PHRASE_TAG),
+	VARIABLE_USE_PHRASE_TAG(EXPRESSION_PHRASE_TAG),
+	STATEMENT_PHRASE_TAG(PHRASE_TAG),
+	SEQUENCE_PHRASE_TAG(STATEMENT_PHRASE_TAG),
+	FIRST_OF_SEQUENCE_PHRASE_TAG(STATEMENT_PHRASE_TAG),
+	DECLARATION_PHRASE_TAG(STATEMENT_PHRASE_TAG),
+	ARGUMENT_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	LABEL_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	LOCAL_VARIABLE_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	LOCAL_CONSTANT_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	MODULE_VARIABLE_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	MODULE_CONSTANT_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	PRIMITIVE_FAILURE_REASON_PHRASE_TAG(DECLARATION_PHRASE_TAG),
+	EXPRESSION_AS_STATEMENT_PHRASE_TAG(STATEMENT_PHRASE_TAG),
+	MACRO_SUBSTITUTION_PHRASE_TAG(PHRASE_TAG),
+	POJO_TAG(TOP_TAG),
+	RAW_FUNCTION_TAG(TOP_TAG),
+	SEMANTIC_RESTRICTION_TAG(TOP_TAG),
+	SET_TAG(TOP_TAG),
+	SET_LINEAR_BIN_TAG(TOP_TAG),
+	SET_HASHED_BIN_TAG(TOP_TAG),
+	TOKEN_TAG(TOP_TAG),
+	LITERAL_TOKEN_TAG(TOKEN_TAG),
+	TUPLE_TAG(TOP_TAG),
+	STRING_TAG(TUPLE_TAG),
+	VARIABLE_TAG(TOP_TAG),
+	TOP_TYPE_TAG(TOP_TAG,
+	TOP_TAG),
+	ANY_TYPE_TAG(TOP_TYPE_TAG),
+	NONTYPE_TYPE_TAG(ANY_TYPE_TAG),
+	SET_TYPE_TAG(NONTYPE_TYPE_TAG,
+	SET_TAG),
+	POJO_TYPE_TAG(NONTYPE_TYPE_TAG,
+	POJO_TAG),
+	NUMBER_TYPE_TAG(NONTYPE_TYPE_TAG,
+	NUMBER_TAG),
+	EXTENDED_INTEGER_TYPE_TAG(NUMBER_TYPE_TAG,
+	EXTENDED_INTEGER_TAG),
+	PHRASE_TYPE_TAG(NONTYPE_TYPE_TAG,
+	PHRASE_TAG),
+	LIST_PHRASE_TYPE_TAG(PHRASE_TYPE_TAG,
+	LIST_PHRASE_TAG),
+	VARIABLE_TYPE_TAG(NONTYPE_TYPE_TAG,
+	VARIABLE_TAG),
+	PRIMITIVE_TYPE_TAG(NONTYPE_TYPE_TAG),
+	FUNCTION_TYPE_TAG(NONTYPE_TYPE_TAG,
+	FUNCTION_TAG),
+	OBJECT_TYPE_TAG(NONTYPE_TYPE_TAG,
+	OBJECT_TAG),
+	MAP_TYPE_TAG(NONTYPE_TYPE_TAG,
+	MAP_TAG),
+	TUPLE_TYPE_TAG(NONTYPE_TYPE_TAG,
+	TUPLE_TAG),
+	CONTINUATION_TYPE_TAG(NONTYPE_TYPE_TAG,
+	CONTINUATION_TAG),
+	RAW_FUNCTION_TYPE_TAG(NONTYPE_TYPE_TAG,
+	RAW_FUNCTION_TAG),
+	FIBER_TYPE_TAG(NONTYPE_TYPE_TAG,
+	FIBER_TAG),
+	META_TAG(ANY_TYPE_TAG,
+	TOP_TYPE_TAG),
+	BOTTOM_TYPE_TAG(ANY_TYPE_TAG);
 
-	TypeTag ()
+	// Special case
+	constructor()
 	{
-		this.depth = 0;
-		this.parent = null;
-		this.highOrdinal = ordinal();
+		depth = 0
+		parent = null
+		highOrdinal = ordinal
 	}
 
-	@SuppressWarnings("ThisEscapedInObjectConstruction")
-	TypeTag (final TypeTag parent)
+	constructor(parent: TypeTag)
 	{
-		assert parent.metaTag == null
-			: "Children of tags with metaTags should also have metaTags";
-		this.depth = parent.depth + 1;
-		this.parent = parent;
-		this.highOrdinal = ordinal();
-		parent.addDescendant(this);
-	}
-
-	TypeTag (final TypeTag parent, final TypeTag instance)
-	{
-		this.depth = parent.depth + 1;
-		this.parent = parent;
-		this.highOrdinal = ordinal();
-		//noinspection ThisEscapedInObjectConstruction
-		parent.addDescendant(this);
-		instance.metaTag = this;
-	}
-
-	final void addDescendant (final TypeTag descendant)
-	{
-		assert descendant.ordinal() == highOrdinal + 1;
-		descendants.add(descendant);
-		highOrdinal++;
-		if (parent != null)
-		{
-			parent.addDescendant(descendant);
+		assert(parent.metaTag === null) { 
+			"Children of tags with metaTags should also have metaTags" 
 		}
+		depth = parent.depth + 1
+		this.parent = parent
+		highOrdinal = ordinal
+		parent.addDescendant(this)
 	}
 
-	final @Nullable TypeTag parent;
-
-	private @Nullable TypeTag metaTag;
-
-	final int depth;
-
-	private final List<TypeTag> descendants = new ArrayList<>();
-
-	private int highOrdinal;
-
-	public TypeTag metaTag ()
+	constructor(parent: TypeTag,
+	instance: TypeTag)
 	{
-		return stripNull(metaTag);
+		depth = parent.depth + 1
+		this.parent = parent
+		highOrdinal = ordinal
+		parent.addDescendant(this)
+		instance.metaTag = this
 	}
 
-	public boolean isSubtagOf (final TypeTag otherTag)
+	fun addDescendant(descendant: TypeTag)
 	{
-		return ordinal() >= otherTag.ordinal()
-			&& highOrdinal <= otherTag.highOrdinal;
+		assert(descendant.ordinal == highOrdinal + 1)
+		descendants.add(descendant)
+		highOrdinal++
+		parent?.addDescendant(descendant)
 	}
 
-	@SuppressWarnings("TailRecursion")
-	TypeTag commonAncestorWith (final TypeTag other)
+	val parent: TypeTag?
+	
+	private var metaTag: TypeTag? = null
+	
+	val depth: Int
+	
+	private val descendants = mutableListOf<TypeTag>()
+	
+	private var highOrdinal: Int
+	
+	fun metaTag(): TypeTag = Nulls.stripNull(metaTag)
+
+	fun isSubtagOf(otherTag: TypeTag): Boolean =
+		(ordinal >= otherTag.ordinal && highOrdinal <= otherTag.highOrdinal)
+
+	fun commonAncestorWith(other: TypeTag?): TypeTag
 	{
 		if (this == other)
 		{
-			return this;
+			return this
 		}
-		final @Nullable TypeTag myParent = this.parent;
-		if (this.depth > other.depth)
+		val myParent = parent
+		if (depth > other!!.depth)
 		{
-			assert myParent != null;
-			return myParent.commonAncestorWith(other);
+			assert(myParent != null)
+			return myParent!!.commonAncestorWith(other)
 		}
-		final @Nullable TypeTag otherParent = other.parent;
-		if (other.depth > this.depth)
+		val otherParent = other.parent
+		if (other.depth > depth)
 		{
-			assert otherParent != null;
-			return otherParent.commonAncestorWith(this);
+			assert(otherParent != null)
+			return otherParent!!.commonAncestorWith(this)
 		}
-		// Depths are equal.
-		assert myParent != null && otherParent != null;
-		assert this != UNKNOWN_TAG && other != UNKNOWN_TAG;
-		return myParent.commonAncestorWith(otherParent);
+		assert(myParent != null && otherParent != null)
+		assert(this != UNKNOWN_TAG && other != UNKNOWN_TAG)
+		return myParent!!.commonAncestorWith(otherParent)
 	}
 
-	static
+	companion object
 	{
-		for (final TypeTag tag : TypeTag.values())
+		init
 		{
-			if (tag.metaTag == null && tag != UNKNOWN_TAG)
+			for (tag in values())
 			{
-				tag.metaTag = stripNull(tag.parent).metaTag;
+				if (tag.metaTag == null && tag != UNKNOWN_TAG)
+				{
+					tag.metaTag = Nulls.stripNull(tag.parent).metaTag
+				}
 			}
-		}
-		BOTTOM_TYPE_TAG.highOrdinal = ANY_TYPE_TAG.ordinal();
-		for (final TypeTag tag : TOP_TYPE_TAG.descendants)
-		{
-			if (!tag.descendants.contains(BOTTOM_TYPE_TAG))
+			BOTTOM_TYPE_TAG.highOrdinal = ANY_TYPE_TAG.ordinal
+			for (tag in TOP_TYPE_TAG.descendants)
 			{
-				tag.descendants.add(BOTTOM_TYPE_TAG);
+				if (!tag.descendants.contains(BOTTOM_TYPE_TAG))
+				{
+					tag.descendants.add(BOTTOM_TYPE_TAG)
+				}
 			}
 		}
 	}
