50af6e4730585b7f8c7b90de10231fa56eaa27d1
Richard Arriaga <richard.arriaga@i3pd.com>
2020-06-10 12:17:05 -0500

    Ported AvailTask All tests passed.

--- src/main/java/com/avail/AvailTask.kt
+++ src/main/java/com/avail/AvailTask.kt
@@ -1,5 +1,5 @@
 /*
- * AvailTask.java
+ * AvailTask.kt
  * Copyright © 1993-2019, The Avail Foundation, LLC.
  * All rights reserved.
  *
@@ -29,45 +29,41 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+package com.avail
 
-package com.avail;
-
-import com.avail.descriptor.fiber.A_Fiber;
-import com.avail.descriptor.fiber.FiberDescriptor;
-import com.avail.descriptor.fiber.FiberDescriptor.ExecutionState;
-import com.avail.descriptor.representation.AvailObject;
-import com.avail.exceptions.PrimitiveThrownException;
-import com.avail.interpreter.execution.Interpreter;
-import kotlin.Unit;
-import kotlin.jvm.functions.Function0;
-
-import javax.annotation.Nullable;
-
-import static com.avail.descriptor.fiber.FiberDescriptor.ExecutionState.ABORTED;
-import static com.avail.descriptor.fiber.FiberDescriptor.ExecutionState.RETIRED;
-import static com.avail.descriptor.fiber.FiberDescriptor.ExecutionState.RUNNING;
-import static com.avail.descriptor.fiber.FiberDescriptor.ExecutionState.SUSPENDED;
-import static com.avail.descriptor.fiber.FiberDescriptor.ExecutionState.TERMINATED;
-import static com.avail.descriptor.fiber.FiberDescriptor.SynchronizationFlag.BOUND;
-import static com.avail.descriptor.fiber.FiberDescriptor.SynchronizationFlag.SCHEDULED;
+import com.avail.descriptor.fiber.A_Fiber
+import com.avail.descriptor.fiber.FiberDescriptor
+import com.avail.descriptor.fiber.FiberDescriptor.ExecutionState
+import com.avail.descriptor.fiber.FiberDescriptor.SynchronizationFlag
+import com.avail.descriptor.representation.AvailObject
+import com.avail.exceptions.PrimitiveThrownException
+import com.avail.interpreter.execution.Interpreter
+import com.avail.interpreter.execution.Interpreter.Companion.current
 
 /**
- * An {@code AvailTask} extends {@link Runnable} with a priority. Instances are intended to be executed only by {@linkplain AvailThread Avail threads}.
+ * An `AvailTask` extends [Runnable] with a priority. Instances are intended to
+ * be executed only by [Avail&#32;threads][AvailThread].
  *
  * @author Todd L Smith &lt;todd@availlang.org&gt;
+ *
+ * @property
+ *      The priority of the [task][AvailTask].  It must be a value in the range
+ *      0..255. @see [quasiDeadline]
+ * @constructor
+ * Construct a new `AvailTask`.
+ *
+ * @param priority
+ *   The desired priority, a long tied to milliseconds since the current epoch.
+ * @param body
+ *   The action to execute for this task.
  */
-public final class AvailTask
-implements Comparable<AvailTask>, Runnable
+class AvailTask constructor(
+	val priority: Int,
+	body: Function0<Unit>) : Comparable<AvailTask>, Runnable
 {
-	/** The action to perform for this task. */
-	private final Function0<Unit> body;
+	/** The action to perform for this task.  */
+	private val body: Function0<Unit>
 
-	/**
-	 * The priority of the {@linkplain AvailTask task}.  It must be a value in the range 0..255.
-	 *
-	 * @see #quasiDeadline
-	 */
-	public final int priority;
 
 	/**
 	 * The quasi-deadline of the task.  This is the moment that the task can no
@@ -77,186 +73,184 @@ implements Comparable<AvailTask>, Runnable
 	 * the future for priority = 0, and places it essentially at the current
 	 * moment for priority = 255.
 	 */
-	private final long quasiDeadline;
+	private val quasiDeadline: Long
 
-	/**
-	 * Construct a new {@code AvailTask}.
-	 *
-	 * @param priority
-	 *        The desired priority, a long tied to milliseconds since the current epoch.
-	 * @param body
-	 *        The action to execute for this task.
-	 */
-	public AvailTask (
-		final int priority,
-		final Function0<Unit> body)
-	{
-		assert priority >= 0 && priority <= 255;
-		this.priority = priority;
-		final long deltaNanos = (1_000_000_000L * (255 - priority)) >> 8;
-		this.quasiDeadline = System.nanoTime() + deltaNanos;
-		this.body = body;
-	}
+	// Note that using Long.compare(long) would deal with counter overflow
+	// in a way we don't want.  Instead, assume a task doesn't sit in the
+	// queue for a century or two(!), and use a technically non-transitive
+	// comparison operation, checking if the difference is negative.
+	// noinspection SubtractionInCompareTo
+	// TODO confirm comment still valid
+	override fun compareTo(other: AvailTask): Int =
+		(quasiDeadline - other.quasiDeadline).compareTo(0)
 
-	/**
-	 * Answer an {@code AvailTask} suitable for resuming the specified {@linkplain FiberDescriptor fiber} using the specified action. If the continuation fails for any reason, then it {@linkplain Interpreter#abortFiber() aborts} the fiber and invokes the {@linkplain AvailObject#failureContinuation() failure continuation} with the terminal {@linkplain Throwable throwable}.
-	 *
-	 * @param fiber
-	 *        A fiber.
-	 * @param body
-	 *        What to do to resume execution of the fiber.
-	 * @return
-	 * An action that sets the execution state of the fiber to {@linkplain ExecutionState#RUNNING running}, binds it to the running {@linkplain AvailThread thread}'s {@linkplain Interpreter interpreter}, and then runs the specified continuation.
-	 */
-	public static Function0<Unit> forFiberResumption (
-		final A_Fiber fiber,
-		final Function0<Unit> body)
-	{
-		assert fiber.executionState().indicatesSuspension();
-		final boolean scheduled =
-			fiber.getAndSetSynchronizationFlag(SCHEDULED, true);
-		assert !scheduled;
-		return () ->
-		{
-			final Interpreter interpreter = Interpreter.current();
-			assert interpreter.fiberOrNull() == null;
-			fiber.lock(() ->
-			{
-				assert fiber.executionState().indicatesSuspension();
-				final boolean bound =
-					fiber.getAndSetSynchronizationFlag(BOUND, true);
-				assert !bound;
-				final boolean wasScheduled =
-					fiber.getAndSetSynchronizationFlag(SCHEDULED, false);
-				assert wasScheduled;
-				fiber.setExecutionState(RUNNING);
-				interpreter.fiber(fiber, "forFiberResumption");
-				return Unit.INSTANCE;
-			});
-			try
-			{
-				body.invoke();
-			}
-			catch (final PrimitiveThrownException e)
-			{
-				// If execution failed, terminate the fiber and invoke its
-				// failure continuation with the throwable.
-				interpreter.adjustUnreifiedCallDepthBy(
-					-interpreter.unreifiedCallDepth());
-				if (!fiber.executionState().indicatesTermination())
-				{
-					assert interpreter.fiberOrNull() == fiber;
-					interpreter.abortFiber();
-				}
-				else
-				{
-					fiber.setExecutionState(ABORTED);
-				}
-				fiber.failureContinuation().invoke(e);
-				fiber.setExecutionState(RETIRED);
-				interpreter.runtime().unregisterFiber(fiber);
-			}
-			catch (final Throwable e)
-			{
-				System.err.println("An unrecoverable VM error has occurred.");
-				throw e;
-			}
-			finally
-			{
-				// This is the first point at which *some other* Thread may have
-				// had a chance to resume the fiber and update its state.
-				final @Nullable Function0<Unit> postExit =
-					interpreter.getPostExitContinuation();
-				if (postExit != null)
-				{
-					interpreter.postExitContinuation(null);
-					postExit.invoke();
-				}
-			}
-			// If the fiber has terminated, then report its result via its
-			// result continuation.
-			fiber.lock(() ->
-			{
-				if (fiber.executionState() == TERMINATED)
-				{
-					fiber.resultContinuation().invoke(fiber.fiberResult());
-					fiber.setExecutionState(RETIRED);
-					interpreter.runtime().unregisterFiber(fiber);
-				}
-				return Unit.INSTANCE;
-			});
-			assert interpreter.fiberOrNull() == null;
-			return Unit.INSTANCE;
-		};
-	}
-
-	/**
-	 * Answer an {@code AvailTask} suitable for performing activities on behalf of an unbound {@linkplain ExecutionState#SUSPENDED} {@linkplain FiberDescriptor fiber}. If the continuation fails for any reason, then it transitions the fiber to the {@linkplain ExecutionState#ABORTED aborted} state and invokes the fiber's {@linkplain AvailObject#failureContinuation() failure continuation} with the terminal {@linkplain Throwable throwable}.
-	 *
-	 * @param fiber
-	 *        A fiber.
-	 * @param continuation
-	 *        What to do to on behalf of the unbound fiber.
-	 * @return
-	 * An action that runs the provided continuation and handles any errors appropriately.
-	 */
-	public static Function0<Unit> forUnboundFiber (
-		final A_Fiber fiber,
-		final Function0<Unit> continuation)
-	{
-		assert fiber.executionState() == SUSPENDED;
-		final boolean scheduled =
-			fiber.getAndSetSynchronizationFlag(SCHEDULED, true);
-		assert !scheduled;
-		return () ->
-		{
-			final boolean wasScheduled =
-				fiber.getAndSetSynchronizationFlag(SCHEDULED, false);
-			assert wasScheduled;
-			try
-			{
-				continuation.invoke();
-			}
-			catch (final Throwable e)
-			{
-				// If execution failed for any reason, then terminate the
-				// fiber and invoke its failure continuation with the
-				// throwable.
-				fiber.setExecutionState(ABORTED);
-				fiber.failureContinuation().invoke(e);
-			}
-			assert Interpreter.current().fiberOrNull() == null;
-			return Unit.INSTANCE;
-		};
-	}
-
-	@Override
-	public int compareTo (final @Nullable AvailTask other)
-	{
-		assert other != null;
-		// Note that using Long.compare(long) would deal with counter overflow
-		// in a way we don't want.  Instead, assume a task doesn't sit in the
-		// queue for a century or two(!), and use a technically non-transitive
-		// comparison operation, checking if the difference is negative.
-		// noinspection SubtractionInCompareTo
-		return Long.compare(quasiDeadline - other.quasiDeadline, 0);
-	}
-
-	@Override
-	public void run ()
+	override fun run()
 	{
 		try
 		{
-			body.invoke();
+			body.invoke()
 		}
-		catch (final Throwable e)
+		catch (e: Throwable)
 		{
 			// Report the exception immediately, then suppress the error.  If we
 			// allowed the error to propagate, it would cause an Interpreter to
 			// be silently lost, and the next creation could overflow the
 			// maximum number of Interpreters.
-			System.err.println("Unexpected internal failure in AvailTask:\n");
-			e.printStackTrace();
+			System.err.println("Unexpected internal failure in AvailTask:\n")
+			e.printStackTrace()
 		}
+	}
+
+	companion object
+	{
+		/**
+		 * Answer an `AvailTask` suitable for resuming the specified
+		 * [fiber][FiberDescriptor] using the specified action. If the
+		 * continuation fails for any reason, then it
+		 * [aborts][Interpreter.abortFiber] the fiber and invokes the [failure
+		 * continuation][AvailObject.failureContinuation] with the terminal
+		 * [throwable][Throwable].
+		 *
+		 * @param fiber
+		 *   A fiber.
+		 * @param body
+		 *   What to do to resume execution of the fiber.
+		 * @return
+		 *   An action that sets the execution state of the fiber to
+		 *   [running][ExecutionState.RUNNING], binds it to the running
+		 *   [thread][AvailThread]'s [interpreter][Interpreter], and then runs
+		 *   the specified continuation.
+		 */
+		fun forFiberResumption(
+			fiber: A_Fiber,
+			body: Function0<Unit>): Function0<Unit>
+		{
+			assert(fiber.executionState().indicatesSuspension())
+			val scheduled =
+				fiber.getAndSetSynchronizationFlag(
+					SynchronizationFlag.SCHEDULED, true)
+			assert(!scheduled)
+			return {
+				val interpreter = current()
+				assert(interpreter.fiberOrNull() == null)
+				fiber.lock {
+					assert(fiber.executionState().indicatesSuspension())
+					val bound =
+						fiber.getAndSetSynchronizationFlag(
+							SynchronizationFlag.BOUND, true)
+					assert(!bound)
+					val wasScheduled =
+						fiber.getAndSetSynchronizationFlag(
+							SynchronizationFlag.SCHEDULED, false)
+					assert(wasScheduled)
+					fiber.setExecutionState(ExecutionState.RUNNING)
+					interpreter.fiber(fiber, "forFiberResumption")
+				}
+				try
+				{
+					body.invoke()
+				}
+				catch (e: PrimitiveThrownException)
+				{
+					// If execution failed, terminate the fiber and invoke its
+					// failure continuation with the throwable.
+					interpreter.adjustUnreifiedCallDepthBy(
+						-interpreter.unreifiedCallDepth())
+					if (!fiber.executionState().indicatesTermination())
+					{
+						assert(interpreter.fiberOrNull() === fiber)
+						interpreter.abortFiber()
+					}
+					else
+					{
+						fiber.setExecutionState(ExecutionState.ABORTED)
+					}
+					fiber.failureContinuation().invoke(e)
+					fiber.setExecutionState(ExecutionState.RETIRED)
+					interpreter.runtime().unregisterFiber(fiber)
+				}
+				catch (e: Throwable)
+				{
+					System.err.println("An unrecoverable VM error has occurred.")
+					throw e
+				}
+				finally
+				{
+					// This is the first point at which *some other* Thread may
+					// have had a chance to resume the fiber and update its
+					// state.
+					val postExit = interpreter.postExitContinuation
+					if (postExit != null)
+					{
+						interpreter.postExitContinuation(null)
+						postExit.invoke()
+					}
+				}
+				// If the fiber has terminated, then report its result via its
+				// result continuation.
+				fiber.lock {
+					if (fiber.executionState() === ExecutionState.TERMINATED)
+					{
+						fiber.resultContinuation().invoke(fiber.fiberResult())
+						fiber.setExecutionState(ExecutionState.RETIRED)
+						interpreter.runtime().unregisterFiber(fiber)
+					}
+				}
+				assert(interpreter.fiberOrNull() == null)
+			}
+		}
+
+		/**
+		 * Answer an `AvailTask` suitable for performing activities on behalf of
+		 * an unbound [ExecutionState.SUSPENDED] [fiber][FiberDescriptor]. If
+		 * the continuation fails for any reason, then it transitions the fiber
+		 * to the [aborted][ExecutionState.ABORTED] state and invokes the
+		 * fiber's [failure continuation][AvailObject.failureContinuation] with
+		 * the terminal [throwable][Throwable].
+		 *
+		 * @param fiber
+		 *   A fiber.
+		 * @param continuation
+		 *   What to do to on behalf of the unbound fiber.
+		 * @return
+		 *   An action that runs the provided continuation and handles any
+		 *   errors appropriately.
+		 */
+		fun forUnboundFiber(
+			fiber: A_Fiber,
+			continuation: Function0<Unit>): Function0<Unit>
+		{
+			assert(fiber.executionState() === ExecutionState.SUSPENDED)
+			val scheduled =
+				fiber.getAndSetSynchronizationFlag(
+					SynchronizationFlag.SCHEDULED, true)
+			assert(!scheduled)
+			return {
+				val wasScheduled =
+					fiber.getAndSetSynchronizationFlag(
+						SynchronizationFlag.SCHEDULED, false)
+				assert(wasScheduled)
+				try
+				{
+					continuation.invoke()
+				}
+				catch (e: Throwable)
+				{
+					// If execution failed for any reason, then terminate the
+					// fiber and invoke its failure continuation with the
+					// throwable.
+					fiber.setExecutionState(ExecutionState.ABORTED)
+					fiber.failureContinuation().invoke(e)
+				}
+				assert(current().fiberOrNull() == null)
+			}
+		}
+	}
+	init
+	{
+		assert(priority in 0 .. 255)
+		val deltaNanos = 1000000000L * (255 - priority) shr 8
+		quasiDeadline = System.nanoTime() + deltaNanos
+		this.body = body
 	}
 }
