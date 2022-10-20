f3a6728cc33e896d409edb2b91d6f267db5123ca
Richard Arriaga <rich@availlang.org>
2022-10-20 15:54:04 -0500

    v2.0.0.alpha10 avail-artifact bug fix

--- avail-stdlib/build.gradle.kts
+++ avail-stdlib/build.gradle.kts
@@ -45,7 +45,7 @@ plugins {
 	id("org.availlang.avail-plugin")
 }
 
-version = "2.0.0.alpha05-1.6.1.alpha03"
+version = "2.0.0.alpha09-1.6.1.alpha04"
 
 avail {
 	projectDescription = "The Avail Standard Library"
--- avail/build.gradle.kts
+++ avail/build.gradle.kts
@@ -52,7 +52,7 @@ val isReleaseVersion =
 dependencies {
 	api("org.availlang:avail-json:1.1.1")
 	api("org.availlang:avail-storage:1.1.1")
-	api("org.availlang:avail-artifact:2.0.0.alpha06")
+	api("org.availlang:avail-artifact:2.0.0.alpha07")
 	AvailModule.addDependencies(this)
 }
 
