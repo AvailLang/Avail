a55bd87514e5740c95759ddf4d1b726df760b2d0
Richard Arriaga <rich@availlang.org>
2023-01-12 17:59:56 -0600

    Fixes to Gradle usage of `avail-artifact` and Avail Gradle plugin.

--- avail/build.gradle.kts
+++ avail/build.gradle.kts
@@ -52,7 +52,7 @@ val isReleaseVersion =
 dependencies {
 	api("org.availlang:avail-json:2.0.0")
 	api("org.availlang:avail-storage:1.1.1")
-	api("org.availlang:avail-artifact:2.0.0.alpha17")
+	api("org.availlang:avail-artifact:2.0.0.alpha18")
 	AvailModule.addDependencies(this)
 }
 
--- build.gradle.kts
+++ build.gradle.kts
@@ -56,7 +56,7 @@ plugins {
 	`maven-publish`
 	publishing
 	id("org.jetbrains.dokka") version "1.7.10" apply false
-	id("org.availlang.avail-plugin") version "2.0.0.alpha17" apply false
+	id("org.availlang.avail-plugin") version "2.0.0.alpha18" apply false
 }
 
 java {
