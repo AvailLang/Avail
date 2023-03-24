da7d8893bbe593a68736c47c1d9b4362432e80ef
Todd L Smith <todd@availlang.org>
2023-03-23 21:00:38 -0500

    Updated library version (to fix a publication issue).

--- avail-stdlib/build.gradle.kts
+++ avail-stdlib/build.gradle.kts
@@ -46,7 +46,7 @@ plugins {
 	id("org.availlang.avail-plugin")
 }
 
-version = "2.0.0.alpha20-1.6.1.alpha09"
+version = "2.0.0.alpha20-1.6.1.alpha10"
 
 avail {
 	projectDescription = "The Avail Standard Library"
