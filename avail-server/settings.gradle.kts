fe1283f165f3382722fd702bd1d999c3196bd1b0
Richard Arriaga <rich@availlang.org>
2023-08-20 21:29:07 -0500

    Patches to `avail-server` and `avail-cli`

--- avail-server/settings.gradle.kts
+++ avail-server/settings.gradle.kts
@@ -8,3 +8,6 @@ pluginManagement {
 rootProject.name = "avail-server"
 include("avail")
 project(":avail").projectDir = File(settingsDir, "../avail")
+include("avail-artifact")
+project(":avail-artifact").projectDir =
+    File(settingsDir, "../avail-artifact")
