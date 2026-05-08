6e6aacce9c026c55f5c1e544f96c3b3ba84fdf9a
Mark van Gulik <mark@availlang.org>
2026-05-08 08:20:46 -0500

    The AbstractAvailObject descriptor field is no longer volatile. - This is a double-digit percentage performance improvement.

--- avail/src/main/kotlin/avail/descriptor/representation/AbstractAvailObject.kt
+++ avail/src/main/kotlin/avail/descriptor/representation/AbstractAvailObject.kt
@@ -59,29 +59,20 @@ abstract class AbstractAvailObject protected constructor(
 	 * semantics. The descriptor essentially says how this object should behave,
 	 * including how its fields are laid out.
 	 *
-	 * TODO – This probably doesn't need to be volatile:
 	 * The only cases where another fiber (thread) can change it is when it's
-	 * not yet shared (i.e., it's mutable or immutable).
-	 *   - becoming immutable;
-	 *   - becoming shared;
-	 *   - becoming an indirection;
-	 *   - updating a padded representation of a tuple for appending;
-	 *   - transforming on an atom to add a field for a properties map;
-	 *   - during creation of a descriptor-per-instance kind, like Pojos,
-	 *     raw functions, and modules;
-	 * All of these uses are fine without volatile access, because the object is
-	 * not yet shared, so other fibers should not be able to observe the change.
-	 * Removing the volatile modifier would allow some CPUs to access the data
-	 * without bus snooping.  Also, HotSpot would be able to optimize away
-	 * redundant reads of a descriptor field if enough called code is inlined,
-	 * which opens up the opportunity for inlining subsequent method invocations
-	 * without conditional guards after the first access.  The infrequent
-	 * non-initializing writes to the field would also be faster, as they can
-	 * get moved to memory asynchronously and even out of order (for different
-	 * writes), only fenced by whatever operation eventually makes a shared
-	 * object visibile to other threads.
+	 * not yet shared (i.e., it's mutable or immutable):
+	 *   - Becoming immutable,
+	 *   - Becoming shared,
+	 *   - Becoming an indirection,
+	 *   - Updating a padded representation of a tuple for appending,
+	 *   - Transforming an atom to add a field for a properties map,
+	 *   - During creation of a descriptor-per-instance kind, like Pojos,
+	 *     raw functions, and modules.
+	 *
+	 * All of these uses are fine without having to add volatile protection,
+	 * because the object is not yet shared, so other fibers should not be able
+	 * to observe the change.
 	 */
-	@field:Volatile
 	var descriptor = initialDescriptor
 
 	/**
